0001e710fc03e505449f8bb68a06ad81bc3b2dd0
VeronightTang <148988540+VeronightTang@users.noreply.github.com>
2025-10-22 10:03:44 +0800

    Update and rename sound_a_song.ino to music_player.ino

--- projects/03_alarm_with_buzzer/music_player.ino
+++ projects/03_alarm_with_buzzer/music_player.ino
@@ -9,7 +9,7 @@ int melody[] = {
   349, 349, 330, 330, 294, 294, 262    
 };
 
-// 每个音符时长（4=四分音符, 8=八分音符）
+// Duration of each note (4 = quarter note, 8 = eighth note)
 int noteDurations[] = {
   4,4,4,4,4,4,2,
   4,4,4,4,4,4,2,
@@ -28,7 +28,7 @@ void loop() {
   for (int i = 0; i < notes; i++) {
     int duration = 1000 / noteDurations[i];
     tone(buzzerPin, melody[i], duration);
-    delay(duration * 1.3); // 控制音符间停顿
+    delay(duration * 1.3); // Control the pause between notes
   }
-  delay(2000); // 播放完等待两秒
+  delay(2000); // Wait two seconds after playing
 }
