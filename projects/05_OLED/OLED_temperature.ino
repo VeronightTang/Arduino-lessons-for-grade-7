68cfcd06f62a7fcac3f528e068e5e256d9e03a7d
VeronightTang <148988540+VeronightTang@users.noreply.github.com>
2025-11-07 09:58:25 +0800

    Update and rename README_DHT.md to OLED_temperature.ino

---
+++ projects/05_OLED/OLED_temperature.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    for(;;); 
+  }
+  
+  display.clearDisplay();
+  display.setTextSize(2);           
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,10);          
+  display.println("Hello");          
+  display.display();                
+}
+
+void loop() {}
+
